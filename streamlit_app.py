5820cca3a2f3d14fabdbf4b7f1eaa4bf627671fb
t3rfms-hia <t3rfms.hia@gmail.com>
2024-11-16 23:44:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ def get_credentials():
     load_dotenv()
 
     # Update the global variables that will be used for authentication in another function
-    globals()["api_key"] = st.secrets["api_key"]
-    globals()["watsonx_project_id"] = st.secrets["project_id"]
+    globals()["api_key"] = os.environ.get('api_key')
+    globals()["watsonx_project_id"] = os.environ.get('project_id')
 
     print("*** Got credentials***")
 
